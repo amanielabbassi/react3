726c8adc66b4472b1224ac1d0f9916f05edf5a75
amanielabbassi <lyemnaabassi@gmail.com>
2021-08-27 23:30:05 +0100

    react3

---
+++ src/component/Profile/ProfilePhoto.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import photo from './photo.jpg'
+const ProfilePhoto = () => {
+    return (
+        <div>
+            <img src={photo} alt=""/> 
+            
+        </div>
+    );
+}
+
+export default ProfilePhoto
